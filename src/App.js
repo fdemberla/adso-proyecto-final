68c3b49d8a843a85ed28ec07213c067ff7929eae
fdemberla <Fernando_Dember@dell.com>
2020-07-15 15:52:30 -0500

    changed basename

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Instalacion from "./Paginas/Instalacion";
+import Contraseña from "./Paginas/Contraseña"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router basename="/adso-proyecto-final/">
+			<div className="wrapper">
+				<nav id="sidebar">
+					<div className="sidebar-header">
+						<h3>Guias</h3>
+					</div>
+
+					<ul className="list-unstyled components">
+						<li>
+							<NavLink
+								className="btn btn-purple text-white"
+								activeClassName="btn btn-secondary"
+								to="/"
+							>
+								Instalacion
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className="btn btn-purple text-white"
+								activeClassName="btn btn-secondary"
+								to="/contraseña"
+							>
+								Contraseña
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+
+				<div id="content">
+					<Switch>
+						<Route exact path="/">
+							<Instalacion />
+						</Route>
+						<Route exact path="/contraseña">
+							<Contraseña />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/Paginas/Contraseña.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contraseña = () => {
+    return (
+        <div>
+            Contraseña
+        </div>
+    );
+};
+
+export default Contraseña;
